3a1293e9e9a72e4c97055fe3dd9d19100cfe2558
loveymelon <52749067+loveymelon@users.noreply.github.com>
2024-04-13 18:19:19 +0900

    Configure UI 구조화

---
+++ RealHealthKing/Protocol/ConfigureProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ConfigureProtocol.swift
+//  RealHealthKing
+//
+//  Created by 김진수 on 4/13/24.
+//
+
+import Foundation
+
+protocol UIConfigureProtocol {
+    func configureUI()
+    func configureHierarchy()
+    func configureLayout()
+}
